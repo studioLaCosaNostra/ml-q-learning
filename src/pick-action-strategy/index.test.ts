602aa654983cd82dc11664af197296466d4f8733
Pawel Laski <zniwus@gmail.com>
2019-03-01 23:47:31 +0100

    update strategy tests

--- src/pick-action-strategy/index.test.ts
+++ src/pick-action-strategy/index.test.ts
@@ -1,5 +1,5 @@
 import '@tensorflow/tfjs-node';
-import { argMax, randomPickAction, greedyPickAction, epsilonGreedyPickAction, softmaxPickAction } from ".";
+import { argMax, randomPickAction, greedyPickAction, epsilonGreedyPickAction, softmaxPickAction, epsilonSoftmaxGreedyPickAction, decayingEpsilonSoftmaxGreedyPickAction } from ".";
 
 describe('Pick action strategy', () => {
   test('argMax', () => {
@@ -26,7 +26,7 @@ describe('Pick action strategy', () => {
   test('greedyPickAction', async () => {
     const index = await greedyPickAction([0, 1, 0]);
     expect(index).toBe(1);
-  })
+  });
 
   test('epsilonGreedyPickAction', async () => {
     const nativeRandom = Math.random;
@@ -36,12 +36,29 @@ describe('Pick action strategy', () => {
     const index2 = await epsilonGreedyPickAction(0.99)([0, 1, 0, 0, 2]);
     expect(index2).toBe(2);
     Math.random = nativeRandom.bind(Math);
-  })
+  });
 
   test('softmaxPickAction', async () => {
+    expect(softmaxPickAction).toBeDefined();
     const actionStats = [1, 2, 3];
     const result = softmaxPickAction(actionStats);
     expect(result).toBeGreaterThanOrEqual(0);
     expect(result).toBeLessThan(actionStats.length);
-  })
+  });
+
+  test('epsilonSoftmaxGreedyPickAction', async () => {
+    expect(epsilonSoftmaxGreedyPickAction).toBeDefined();
+    const actionStats = [1, 2, 3];
+    const result = epsilonSoftmaxGreedyPickAction(0.5)(actionStats);
+    expect(result).toBeGreaterThanOrEqual(0);
+    expect(result).toBeLessThan(actionStats.length);
+  });
+
+  test('decayingEpsilonSoftmaxGreedyPickAction', async () => {
+    expect(decayingEpsilonSoftmaxGreedyPickAction).toBeDefined();
+    const actionStats = [1, 2, 3];
+    const result = decayingEpsilonSoftmaxGreedyPickAction(0.5)(actionStats, 1000);
+    expect(result).toBeGreaterThanOrEqual(0);
+    expect(result).toBeLessThan(actionStats.length);
+  });
 });
